2734207b88163f3b471fa747bd819d43a1678039
Eric20Junior <eric20junior@gmail.com>
2023-11-02 18:16:22 +0100

    vercel config setup

--- codestack/settings.py
+++ codestack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd@lfw+7lk1nz-sh9an-)qp!pdew#0d&!g@l57pc9fe5-8s0l7q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
